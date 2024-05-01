ad0dc9210c8a3f8b4d837bbf1b87ece2f3380104
asikharulidze <ana.sixarulidze@gmail.com>
2024-05-01 10:42:41 +0400

    .

--- src/app/auth/register/page.js
+++ src/app/auth/register/page.js
@@ -1,12 +1,12 @@
 "use client";
-
+import { useRouter } from "next/navigation";
 import Register from "@/auth/Register";
 import Modal from "@/components/Modal/Modal";
 
 export default function registerPage() {
-
+  const router = useRouter();
   return (
-    <Modal>
+    <Modal onClose={() => router.back()}>
       <Register />
     </Modal>
   );
